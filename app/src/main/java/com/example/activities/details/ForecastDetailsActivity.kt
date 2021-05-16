c71ee7f252af69ed650649d0bd35aa55c193dc60
sengab87 <sengab87@gmail.com>
2021-05-17 00:48:27 +0200

    First Commit

---
+++ app/src/main/java/com/example/activities/details/ForecastDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.activities.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForecastDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forecast_details)
+    }
+}
